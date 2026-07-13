0c6d8b3cf1fa4db67c5409417b281e4b60b3a4a5
shani <gajerasunny15@gmail.com>
2026-07-13 13:35:40 +0530

    style: fix background image cropping in values section by aligning to edges

--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -48,8 +48,8 @@ export default function Values() {
 
       {/* Image Decorative Circles */}
       <div className="absolute inset-0 overflow-hidden pointer-events-none z-0">
-        <img src="/Group 7.png" alt="" className="absolute top-1/2 -translate-y-1/2 -left-[250px] w-[550px] object-contain opacity-100" />
-        <img src="/Group 9.png" alt="" className="absolute top-[80px] -right-[120px] w-[250px] object-contain opacity-100" />
+        <img src="/Group 7.png" alt="" className="absolute top-1/2 -translate-y-1/2 left-0 h-[550px] w-auto object-contain opacity-100" />
+        <img src="/Group 9.png" alt="" className="absolute top-[80px] right-0 h-[350px] w-auto object-contain opacity-100" />
       </div>
 
       <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-8 lg:px-12">
